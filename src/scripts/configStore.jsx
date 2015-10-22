cc6c8e62bf133e52c7d9348ff0c6687023862a53
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-10-22 18:36:01 +0100

    Fix configStore file

--- src/scripts/configStore.jsx
+++ src/scripts/configStore.jsx
@@ -2,16 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'middlewares/promiseMiddleware';
 import * as reducers from 'reducers/';
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
-
 export default function configureStore() {
-    const store = createStoreWithMiddleware(combineReducers(reducers));
-
-    if (module.hot) {
-        module.hot.accept('./reducers/index', () =>
-            store.replaceReducer(require('./reducers/index'))
-        );
-    }
-
-    return store;
+	const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+    return createStoreWithMiddleware(combineReducers(reducers));
 }

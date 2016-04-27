eeadee570c45bf97fd6a83136511475d8adfb145
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-04-27 14:44:08 +0200

    chore(*): upgrade react-native and redux-thunk

---
+++ src/scripts/routes.js
@@ -0,0 +1,12 @@
+const routes = {};
+
+routes.getHomeRoute = () => ({
+  getSceneClass() {
+    return require('@components/Welcome/').default;
+  },
+  getTitle() {
+    return 'Welcome';
+	}
+});
+
+export default routes;

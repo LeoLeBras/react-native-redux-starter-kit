dc05fc510bc50cca7782054fef4f3a5e76b99f76
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-05-16 22:49:15 +0200

    chore(*): ignore yellow boxes from react-native-router-flux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-native-router-flux'
+import ignoreYellowBoxes from '@utils/ignoreYellowBoxes'
 import routes from '@routes/app'
 import createStore from '@store/create'
+
+ignoreYellowBoxes()
 
 const Kernel = () => (
   <Provider store={createStore()}>
---
+++ src/utils/ignoreYellowBoxes.js
@@ -0,0 +1,3 @@
+export default function ignoreYellowBoxes() {
+  console.ignoredYellowBox = ['Warning: ReactNative.']
+}

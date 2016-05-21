528a55a374d439d718ced411760c2f755b72f668
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-05-21 12:29:21 +0200

    fix(*): fix api config

---
+++ config/index.js
@@ -0,0 +1,1 @@
+export const API = 'https://api.github.com'
--- src/api/create.js
+++ src/api/create.js
@@ -1,9 +1,10 @@
 /* @flow */
 
 import { create } from 'apisauce'
+import { API } from '@config'
 
 const api = create({
-  baseURL: 'https://example.com',
+  baseURL: API,
 })
 
 export default api
